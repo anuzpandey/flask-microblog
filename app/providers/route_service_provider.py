24783266d812ce07448c5414a29ad81e1518034e
AnuzPandey <anuzbvbmaniac123@gmail.com>
2024-08-05 00:08:25 +0545

    :zap: feat: update last_seen attribute with before_request decorator from flask.:

--- app/providers/route_service_provider.py
+++ app/providers/route_service_provider.py
@@ -1,2 +1,14 @@
+from datetime import datetime, timezone
+
+from flask_login import current_user
+
+from app import app, db
 from app.routes import authentication  # noqa: F401 # This is added to resolve unused import warning
 from app.routes import web  # noqa: F401 # This is added to resolve unused import warning
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
